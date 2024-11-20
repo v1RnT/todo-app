586fe6fb38d7569336727bed1b36d11db9e9697d
Vitalii <mlineckiyvitaliy@gmail.com>
2024-11-21 00:38:47 +0200

    implemented todo app

--- README.md
+++ README.md
@@ -1,9 +1,75 @@
-# React TypeScript Starter Pack
+# Todo App
 
-To use this template click `Use this template`
+This project is a Todo application that allows users to manage their tasks effectively. Including the functionality for adding, editing, filtering, marking as completed your tasks.
 
-### Available Scripts
+You can check out the page by the link: \
+[Demo Link](https://v1rnt.github.io/todo-app/)
 
-`Deploy` - available to deploy your application to gh-pages
+## Features
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+**Add Tasks**: Add new tasks to the list. \
+**Edit Tasks**: Update task details. \
+**Mark as Complete**: Toggle tasks as completed or uncompleted. \
+**Filter Tasks**: View tasks based on their status (All, Active, Completed). \
+**Delete Tasks**: Remove tasks individually or in bulk. \
+**Error Handling**: Displays user-friendly error messages in case of API issues.
+
+## Technologies Used
+
+**React**: Component-based UI library. \
+**TypeScript**: Static type checking for JavaScript. \
+**CSS/SCSS**: Styling for the application. \
+**Fetch API**: For making asynchronous requests to interact with the backend APIs. \
+**React Hooks**:
+
+- **useState** for managing state.
+- **useEffect** for handling side effects like data fetching.
+- **useMemo** and useCallback for performance optimization.
+
+## API Integration
+
+The app uses three API endpoints:
+
+GET /todos - Fetches all todos from the server. \
+DELETE /todos/:id - Deletes a specific todo. \
+PATCH /todos/:id - Updates a specific todo.
+
+## Installation
+
+Clone the repository:
+
+```bash
+git clone https://github.com/v1RnT/todo-app.git
+```
+
+Navigate to the directory:
+
+```bash
+cd todo-app
+```
+
+Before the installation, make sure to set the right version for Node 20:
+
+```bash
+node -v
+```
+
+After u made sure u have Node 20, feel free to install all dependencies:
+
+```bash
+npm i
+```
+
+After installation, check the page using:
+
+```bash
+npm start
+```
+
+## Contacts
+
+Feel free to reach out if you have any questions or feedback:
+
+Telegram: [@v1rnt](https://t.me/v1RnT) \
+LinkedIn: [Vitalii Mlynetskyi](https://www.linkedin.com/in/vitalii-mlynetskyi-62823727b/) \
+Email: mlynetskyivitalii@gmail.com
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,170 @@
-import React from 'react';
-import './App.scss';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { deleteTodo, getTodos, patchTodo } from './api/todos';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
+import { Errors } from './types/Errors';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { ErrorMessage } from './components/ErrorMessage';
+
+import { handleError } from './utils/handleError';
+import { getFilteredTodos } from './utils/getFilteredTodos';
 
 export const App: React.FC = () => {
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState<Errors>(Errors.Default);
+  const [filterOption, setFilterOption] = useState<Filter>(Filter.All);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [deletionIds, setDeletionIds] = useState<number[]>([]);
+  const [pendingTodos, setPendingTodos] = useState<Todo[]>([]);
+
+  const completedTodoIds = useMemo(() => {
+    return todosFromServer.filter(todo => todo.completed).map(todo => todo.id);
+  }, [todosFromServer]);
+
+  const uncompletedTodos = useMemo(() => {
+    return todosFromServer.filter(todo => !todo.completed);
+  }, [todosFromServer]);
+
+  const uncompletedTodosAmount = uncompletedTodos.length;
+
+  const filteredTodos = useMemo(() => {
+    return getFilteredTodos(todosFromServer, filterOption);
+  }, [todosFromServer, filterOption]);
+
+  const handleDeleteTodos = useCallback((ids: number[]) => {
+    Promise.all(
+      ids.map(id => {
+        setDeletionIds(current => [...current, id]);
+
+        deleteTodo(id)
+          .then(() => {
+            setTodosFromServer(current =>
+              current.filter(todo => todo.id !== id),
+            );
+          })
+          .catch(() => {
+            handleError(setErrorMessage, Errors.DeleteTodo);
+          })
+          .finally(() =>
+            setDeletionIds(currIds => currIds.filter(currId => currId !== id)),
+          );
+      }),
+    );
+  }, []);
+
+  const handleChangeTodos = useCallback(
+    (newTodos: Todo[]) => {
+      return Promise.all(
+        newTodos.map(todo => {
+          setPendingTodos(current => [...current, todo]);
+
+          const { id, ...todoBody } = todo;
+
+          return patchTodo(todoBody, id)
+            .then(() => {
+              setTodosFromServer(current =>
+                current.map(currentTodo => {
+                  return currentTodo.id !== todo.id ? currentTodo : todo;
+                }),
+              );
+            })
+            .catch(() => {
+              handleError(setErrorMessage, Errors.UpdateTodo);
+              throw new Error(errorMessage);
+            })
+            .finally(() => setPendingTodos([]));
+        }),
+      );
+    },
+    [errorMessage],
+  );
+
+  const handleToggleCompleted = (id: number, updatedField: Partial<Todo>) => {
+    setPendingTodos(current => [...current]);
+
+    return patchTodo(updatedField, id)
+      .then((updatedTodo: Todo) =>
+        setTodosFromServer(current =>
+          current.map(todo => (todo.id === id ? updatedTodo : todo)),
+        ),
+      )
+      .catch(() => {
+        handleError(setErrorMessage, Errors.UpdateTodo);
+        throw new Error(errorMessage);
+      })
+      .finally(() =>
+        setPendingTodos(current => current.filter(todo => todo.id !== id)),
+      );
+  };
+
+  const handleToggleCompletedAll = () => {
+    if (uncompletedTodos && uncompletedTodosAmount) {
+      Promise.allSettled(
+        uncompletedTodos.map(({ id }) =>
+          handleToggleCompleted(id, { completed: true }),
+        ),
+      );
+    } else {
+      Promise.allSettled(
+        todosFromServer.map(({ id }) =>
+          handleToggleCompleted(id, { completed: false }),
+        ),
+      );
+    }
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodosFromServer)
+      .catch(() => handleError(setErrorMessage, Errors.LoadingTodos));
+  }, []);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          setTodos={setTodosFromServer}
+          setError={setErrorMessage}
+          setTempTodo={setTempTodo}
+          tempTodo={tempTodo}
+          uncompletedTodosAmount={uncompletedTodosAmount}
+          todos={todosFromServer}
+          onToggleCompletedAll={handleToggleCompletedAll}
+        />
+
+        {!!todosFromServer.length && (
+          <>
+            <TodoList
+              todos={filteredTodos}
+              tempTodo={tempTodo}
+              deletionIds={deletionIds}
+              pendingTodos={pendingTodos}
+              errorMessage={errorMessage}
+              onTodosChange={handleChangeTodos}
+              onDeleteTodos={handleDeleteTodos}
+            />
+            <Footer
+              filterOption={filterOption}
+              completedTodoIds={completedTodoIds}
+              uncompletedTodosAmount={uncompletedTodosAmount}
+              setFilterOption={setFilterOption}
+              setDeletionIds={setDeletionIds}
+              onDeleteTodos={handleDeleteTodos}
+            />
+          </>
+        )}
+      </div>
+
+      <ErrorMessage
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
     </div>
   );
 };
